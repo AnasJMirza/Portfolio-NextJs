61305404dd819a74f397825ae406306e405fbbb3
AnasJMirza <ajmhimself@gmail.com>
2022-07-16 04:29:35 +0500

    portfolio with react bricks

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,19 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 border-t py-12 h-32">
+    <footer className="bg-gray-50 border-t py-12">
       <div className="max-w-5xl mx-auto flex justify-between">
         <div className="flex items-center space-x-4">
-          <img
+          {/* <img
             src="/react-bricks-icon.svg"
             alt="React Bricks"
             className="w-8"
-          />
-          <div className="uppercase text-sm tracking-wider">Next.js website starter</div>
+          /> */}
+          <div className="uppercase text-sm tracking-wider">Made With Love by Anas J Mirza 🔥</div>
         </div>
         <div className="text-gray-500 text-sm">
           © {new Date().getFullYear()}{' '}
           <a href="https://reactbricks.com" className="hover:text-pink-600">
-            React Bricks
+            All Rights Reserverd
           </a>
         </div>
       </div>
